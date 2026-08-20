1c23374f76e62ab7fb23a1ec92f2fcd9cceee33b
Ron Hedwig Zape <ron@ronhedwigzape.com>
2026-08-21 05:06:05 +0800

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable = 1.1
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "Hello"
+print(type(variable))
+
+variable = [1, 2, 3]
+print(type(variable))

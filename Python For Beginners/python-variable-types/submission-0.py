8229117bc199821cc2c6705782629ddfd5810ca7
Ron Hedwig Zape <ron@ronhedwigzape.com>
2026-08-20 12:37:47 +0800

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 3
+float_type = 3.3
+bool_type = True
+string_type = "bobo"
+list_type = ['d','2','4']
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
